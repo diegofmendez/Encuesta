add6e1566276311c4ac16c9f069fca50aeeedf94
estudiante <estudiante@10.220.4.119>
2025-04-22 17:31:59 -0500

    commit 1 diegomendez

---
+++ Entrega2/src/entrega2/Entrega2.java
@@ -0,0 +1,21 @@
+
+package entrega2;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Entrega2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        System.out.println("Hola");
+    }
+    
+}
